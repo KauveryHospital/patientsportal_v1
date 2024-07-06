3a2be0d78e668e29b088d69d75a1c19ac8c1eca7
Vejaysundaram-Rajkumar <vejaysundaramrajkumar@gmail.com>
2024-07-06 14:32:00 +0530

    Home page modification

--- src/components/HomeStyles.styles.js
+++ src/components/HomeStyles.styles.js
@@ -1,3 +1,7 @@
+import { height } from "@fortawesome/free-solid-svg-icons/fa0";
+import { textFieldClasses } from "@mui/material";
+import Images from './constants/Images.js';
+
 const styles = {
     root: {
         backgroundColor: '#EFEFEF',
@@ -10,7 +14,6 @@ const styles = {
         width: '100%',
         height: '80px',
         backgroundColor: '#FFFFFF',
-        boxShadow: '0px 3px 3px #00000029',
         alignItems: 'center',
         padding: '0px',
         justifyContent: 'space-between',
@@ -22,18 +25,20 @@ const styles = {
         width: '100%',
     },
     logo: {
-        height: '50px',
-        marginRight: '20px',
+        height: '70px',
+        marginLeft: '20px',
     },
     locationSection: {
         display: 'flex',
+        color: '#962067',
+        textAlign: "left",
         alignItems: 'center',
-        color: '#962067'
+        font: "normal normal normal 18px/27px Poppins",
+        letterSpacing: "0px",
+        width: '24px',
+        height: '35px',
     },
-    dropdown: {
-        marginLeft: '10px',
-        marginRight: '10px',
-    },
+    
     instructions: {
         flex: 1,
         display: 'flex',
@@ -43,7 +48,8 @@ const styles = {
     },
     ins1: {
         fontFamily: 'Poppins',
-        fontSize: '32px',
+        paddingTop:'10px',
+        fontSize: '50px',
         lineHeight: '40px',
         letterSpacing: '0px',
         color: '#962067',
@@ -55,7 +61,7 @@ const styles = {
     },
     ins2: {
         fontFamily: 'Poppins',
-        fontSize: '22px',
+        fontSize: '30px',
         lineHeight: '30px',
         letterSpacing: '0px',
         color: '#58595B',
@@ -67,7 +73,7 @@ const styles = {
     },
     ins3: {
         fontFamily: 'Poppins',
-        fontSize: '18px',
+        fontSize: '22px',
         lineHeight: '24px',
         letterSpacing: '0px',
         color: '#939598',
@@ -77,19 +83,51 @@ const styles = {
             lineHeight: '20px',
         },
     },
+
     searchBar: {
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
-        margin: '20px 0',
+        alignItems: 'center',  
         width: '100%',
+        backgroundImage:'{Images.family_front}',
     },
-    searchInput: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-        borderColor: '#962067',
-        margin: '0 10px',
+    nav:{
+        position:'relative',
+        left:'68%',
     },
+        navul: {
+        listStyle: 'none',
+        marginTop:'5px',
+        fontSize:'100px',
+        padding: 0,
+        display: 'flex',
+        justifyContent: 'flex-left',
+      },
+      
+      navli: {
+        marginLeft: '20px',
+        fontWeight: 'bold',
+        fontSize: '1px',
+        fontFamily:  'sans-serif',
+      },
+      
+    nava:{
+        color: '#fff',
+        textDecoration:' none',
+        fontSize: '18px',
+      },
+      
+      
+    serachtext:{
+        position:'relative',
+        bottom:'60%',
+        left:'40%',
+        letterSpacing:' 0px',
+        color: '#58595B',
+        opacity: 1,
+        width:'50%',
+    },
+
     locationDropdown: {
         flex: 1,
         backgroundColor: '#FFFFFF',
@@ -97,110 +135,235 @@ const styles = {
         margin: '0 10px',
     },
     searchButton: {
-        flexShrink: 0,
-        height: '55px',
-        background: '#962067',
-        color: '#FFFFFF',
+
+        width: '300px',
+        margin: '0 20px',
+        height: '50px',
+        background: '#FFFFFF 0% 0% no-repeat padding-box',
         boxShadow: '0px 3px 6px #00000029',
-        borderRadius: '26px',
-        margin: '0 10px',
+        border: '1px solid #E9AFB7',
+        fontFamily:'Poppins',
+        borderRadius: '35px',
+        color:'#58595B',
+        fontsize:'26px',
+        fontweight:'regular',
+        opacity: 1
     },
+    searchmain: {
+        background:' #962067 0% 0% no-repeat padding-box',
+        color:'white',
+        width: '70px',
+        height: '28px',
+        boxShadow: '0px 3px 6px #00000029',
+        fontStyle:'Poppins',
+        fontFamily:'Poppins',
+        borderRadius:' 26px',
+        position:'absolute',
+        left:'30%',
+        opacity: 1,
+
+    },
+    
+    search: {
+        position:'relative',
+        width:'100%',
+        display: 'flex',
+        alignItems: 'center',
+    // add other styles as needed
+  },
+  searchContainer: {
+    position: 'relative', // Make the container relative for positioning the icon
+    width: '100%', // Ensure the container spans the full width
+  },
+  searchfield: {
+    width: '70%',
+    padding: '10px',
+    borderColor:'#962067',
+    position:'relative',
+    right:'50%',
+background: '#FFFFFF 0% 0% no-repeat padding-box',
+border: '1px solid #962067',
+borderRadius: '35px',
+opacity: 1,
+  },
+  searchIcon: {
+    height: '1.5rem',
+    width: '1.5rem',
+    padding: '4px',
+    position: 'absolute',
+    alignItems:'right',
+    boxSizing: 'border-box',
+    top: '50%',
+    left: '2px',
+    transform: 'translateY(-50%)',
+  },
+
     secondSplit: {
         display: 'flex',
         width: '100%',
         backgroundColor: '#FFF7FB',
         justifyContent: 'space-between',
-        padding: '20px',
-        marginTop: '5px',
         flexWrap: 'wrap',
+    },
+    buttext:{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent:'center',
+        textAlign: 'center',
+        width: '100%',
+        height: '55px',
+        borderRadius: '35px',
+        opacity: 1,
+        font: 'normal normal normal 17px Poppins',
+        letterSpacing: '0px'    ,
+    },
+    qrtext:{
+        display: 'flex',
+        alignItems: 'left',
+        position: 'relative',
+        right:'15%',
+        width: '60%',
+        height: '55px',
+        borderRadius: '35px',
+        opacity: 1,
+        font: 'normal normal normal 17px Poppins',
+        letterSpacing: '0px'    ,
     },
     imageSection: {
         flex: 1,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        margin: '20px 0',
+        margin: '0 0',
     },
-    menuIcons: {
-        display: 'flex',
-        justifyContent: 'space-around',
-        width: '100%',
-        marginBottom: '10px',
-    },
-    image: {
-        width: '100%',
-        height: 'auto',
+
+    qrimage:{
+        width: '5%',
+        borderWidth: '10px',
+        marginLeft: '10%',
+        display: 'block',            // Change to block if you want it to behave like a block-level element
+        alignSelf: 'flex-start',
     },
     thirdSplit: {
         display: 'flex',
-        width: '100%',
+        width: '95%',
         backgroundColor: '#FFFFFF',
         borderRadius: '10px',
-        padding: '20px',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        marginTop: '8px',
+        alignItems:'center',
+        marginTop: '2%',
+        marginLeft:'3%',
+        marginRight: '4%',
+        
     },
-    zigZag: {
-        flex: 1,
+    tt1: {
+        fontFamily: 'Poppins',
+        paddingTop:'10px',
+        fontSize: '40px',
+        lineHeight: '40px',
         display: 'flex',
-        flexDirection: 'column',
+        justifyContent: 'center',
+        letterSpacing: '0px',
+        color: '#962067',
+        opacity: 1,
+        '@media (max-width: 600px)': {
+            fontSize: '24px',
+            lineHeight: '30px',
+        },
     },
-    zigZagRow: {
+    tt2: {
+        fontFamily: 'Poppins',
+        fontSize: '30px',
+        lineHeight: '30px',
         display: 'flex',
-        alignItems: 'center',
-        marginBottom: '20px',
+        justifyContent: 'center',
+
+        letterSpacing: '0px',
+        color: '#58595B',
+        opacity: 1,
+        '@media (max-width: 600px)': {
+            fontSize: '18px',
+            lineHeight: '24px',
+        },
     },
-    zigZagImage: {
-        marginRight: '10px',
-    },
-    scanners: {
+    tt3: {
+        fontFamily: 'Poppins',
         display: 'flex',
-        justifyContent: 'space-around',
-        marginBottom: '20px',
+        justifyContent: 'center',
+
+        fontSize: '22px',
+        lineHeight: '24px',
+        letterSpacing: '0px',
+        color: '#939598',
+        opacity: 1,
+        '@media (max-width: 600px)': {
+            fontSize: '16px',
+            lineHeight: '20px',
+        },
     },
-    scanner: {
-        width: '150px',
-        height: '150px',
+    eventCalander:{
+    width: '237px',
+    height: '237px',
+    position: 'relative',
+    left:'43%',
+    opacity: 1,
     },
-    rightContent: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        padding: '20px',
-    },
-    cards: {
-        display: 'flex',
-        justifyContent: 'space-around',
-        marginBottom: '20px',
-        flexWrap: 'wrap',
-    },
-    card: {
-        width: '45%',
-        marginBottom: '20px',
-    },
-    videos: {
-        display: 'flex',
-        justifyContent: 'space-around',
-        width: '100%',
-        flexWrap: 'wrap',
-    },
-    video: {
-        width: '45%',
-        height: '300px',
-        marginBottom: '20px',
-    },
+
     fourthSplit: {
         display: 'flex',
-        width: '100%',
-        backgroundColor: '#FFFFFF',
+        width: 'fit-screen',
+        backgroundColor: '#962067',
         borderRadius: '10px',
         padding: '20px',
         flexWrap: 'wrap',
         justifyContent: 'space-between',
         marginTop: '8px',
     },
+
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: 'fit-screen',
+        color:'#FFFFFF',
+      },
+      featurename: {
+        color:'#E9AFB7',
+        fontFamily:'Poppins',
+        fontSize:'20px',
+      },
+      gridContainer: {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(4, 1fr)',
+        marginTop: '16px',
+      },
+      featureBox: {
+        padding: '16px',
+        textAlign: 'center',
+        borderRadius: '8px',
+        
+      },
+      imagecont: {
+        width: '200px',
+        height: '200px',
+        backgroundColor: 'white',
+        borderRadius: '50%',
+        overflow: 'hidden',
+        marginBottom: '50px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      image: {
+        maxWidth: '60%',
+        height: '60%',
+        
+        objectFit: 'cover', // Ensures the image covers the entire container
+      },
+
+
+
+
     cardGrid: {
         display: 'flex',
         flexWrap: 'wrap',
--- src/components/Homedumm.js
+++ src/components/Homedumm.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { AppBar, Toolbar, Typography, MenuItem, Select, IconButton, TextField, Button, Box, Card, CardContent, Container } from '@mui/material';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import { AppBar, Toolbar, Typography, MenuItem, Select, IconButton, Button, Box, Card, CardContent, Container } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { faBell} from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
-import styles from './HomeStyles.styles';
-import Images from '../constants/Images';
+import styles from './HomeStyles.styles.js';
+import Images from './constants/Images.js';
 
 const Home = () => {
     const [dropdownOptions, setDropdownOptions] = useState([]);
@@ -42,98 +43,129 @@ const Home = () => {
                             ))}
                         </Select>
                     </Box>
-                    <IconButton edge="end" color="inherit">
-                        <NotificationsIcon />
+                    <IconButton color="inherit">
+                    <FontAwesomeIcon icon={faBell}  style={{ color: '#962067' }} />
                     </IconButton>
                 </Toolbar>
             </AppBar>
 
             {/* Second Split */}
             <Box style={styles.secondSplit}>
+            <nav style={styles.nav}>
+        <ul style={styles.navul}>
+          <li><a href="#home" style={styles.navli}> <img src={Images.home} alt="home" />Home</a> </li>
+          <li> <img src={Images.consult}/> <a href="#" style={styles.navli}  Consult /></li>
+          <li> <img src={Images.MHCIcon}/> <a href="#" style={styles.navli}  MHC  /></li>
+          <li><img src={Images.records}/><a href="#" style={styles.navli} Record  /></li>
+          <li> <img src={Images.profile}/><a href="#" style={styles.navli}Login  /></li>
+        </ul>
+      </nav>
                 <Box style={styles.instructions}>
-                    <Typography variant="body1" style={styles.ins1}>Welcome to Patient Portal!</Typography>
+                    <Typography variant="body1" style={styles.ins1}>Welcome to Kauvery Kare!</Typography>
+                    <br></br>
+                    <br></br>
                     <Typography variant="body1" style={styles.ins2}>Hey there, Kauvery cares.</Typography>
+                    <br></br>
                     <Typography variant="body1" style={styles.ins3}>Your Healthcare, just a click away!</Typography>
                 </Box>
                 <Box style={styles.searchBar}>
-                    <Select
-                        variant="outlined"
-                        placeholder="Location"
-                        style={styles.locationDropdown}
-                        IconComponent={ArrowDropDownIcon}
-                    >
-                        {dropdownOptions.map(option => (
-                            <MenuItem key={option.id} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <TextField
-                        variant="outlined"
-                        placeholder="Search"
-                        InputProps={{
-                            style: {
-                                backgroundColor: '#FFFFFF',
-                                borderColor: '#962067',
-                            }
-                        }}
-                        style={styles.searchInput}
-                    />
-                    <Button style={styles.searchButton}>Search Doctor</Button>
+                <Button style={styles.searchButton} startIcon={<img src={Images.appointment} alt="Appointment" />}>Book an Appointment</Button>  
+                <Button style={styles.searchButton} startIcon={<img src={Images.healthCheckup} alt="Health checkup" />}>Book Health checkups</Button>
                 </Box>
                 <Box style={styles.imageSection}>
-                    <Box style={styles.menuIcons}>
-                        {[...Array(5)].map((_, index) => (
-                            <IconButton key={index}><ArrowDropDownIcon /></IconButton>
-                        ))}
-                    </Box>
-                    <img src="/path/to/image.jpg" alt="Right Image" style={styles.image} />
+                <Typography  variant="body1" style={styles.buttext}>
+                    Book for In-hospital or Video Consultation  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Book online to avail offers  </Typography>   
+                    <img src={Images.qr} alt="Right" style={styles.qrimage} />
+                    
                 </Box>
+<br></br>
+                        
+                <Box style={styles.searchBar}> 
+                <Typography  variant="body1" style={styles.serachtext}>
+                What service can we help you find?</Typography>  
+                    
+                <Typography  variant="body1" style={styles.qrtext}>
+                Download our Mobile app </Typography>  
+                        
+                <div style={styles.search} >
+                <input type="search" style={styles.searchfield} placeholder="Search by Specialty, Condition or Doctor Name" />
+                <img src={Images.searchIcon} alt="Right" style={styles.searchIcon} />
+                <Button style={styles.searchmain}>Search</Button>
+      </div>
+      
+      </Box>
+
             </Box>
 
             {/* Third Split */}
             <Box style={styles.thirdSplit}>
-                {/* Zig-zag content */}
-                <Box style={styles.zigZag}>
-                    {[...Array(4)].map((_, index) => (
-                        <Box key={index} style={styles.zigZagRow}>
-                            <img src="/path/to/zigzag-image.jpg" alt={`Zigzag ${index}`} style={styles.zigZagImage} />
-                            <Typography variant="body1">Instruction for zigzag {index + 1}</Typography>
-                        </Box>
-                    ))}
+                
+            <Box style={styles.instructions}>
+                    <Typography variant="body1" style={styles.tt1}>Upcoming events</Typography>
+                    <br></br>
+                    <br></br>
+                    <Typography variant="body1" style={styles.tt2}>No events!</Typography>
+                    <br></br>
+                    <Typography variant="body1" style={styles.tt3}>Please login to your account to stay updated on your upcoming events</Typography>
+                    <img src={Images.eventCalander} alt="Right" style={styles.eventCalander} />
                 </Box>
-                {/* Scanners */}
-                <Box style={styles.scanners}>
-                    <img src="/path/to/appstore-scanner.png" alt="App Store" style={styles.scanner} />
-                    <img src="/path/to/playstore-scanner.png" alt="Play Store" style={styles.scanner} />
-                </Box>
-                {/* Right Side */}
-                <Box style={styles.rightContent}>
-                    <Box style={styles.cards}>
-                        {[...Array(2)].map((_, index) => (
-                            <Card key={index} style={styles.card}>
-                                <CardContent>
-                                    <Typography variant="h5">Card {index + 1}</Typography>
-                                    <Typography variant="body2">Card {index + 1} content</Typography>
-                                </CardContent>
-                            </Card>
-                        ))}
-                    </Box>
-                    <Box style={styles.videos}>
-                        {[...Array(2)].map((_, index) => (
-                            <iframe
-                                key={index}
-                                title={`video${index + 1}`}
-                                src={`https://www.youtube.com/embed/${index + 1}`}
-                                style={styles.video}
-                            />
-                        ))}
-                    </Box>
-                </Box>
+                           
             </Box>
+{/* Fourth Split features */}
+<Box style={styles.fourthSplit}>
 
+    <Box style={styles.container}>
+      <Typography variant="h4" gutterBottom>
+      Our Features
+      </Typography>
+
+      <Box style={styles.gridContainer}>
+        {/* Feature 1 */}
+        <Box style={styles.featureBox}>
+            <div style={styles.imagecont}>
+          <img  style={styles.image}src={Images.hospitalVisit} alt="Feature 1" /></div>
+          <Typography variant="h6" style={styles.featurename}>Book Consultation</Typography>
+          <Typography variant="body1">
+          Book an online consultation or hospital visit with our in house specialties.
+          </Typography>
+        </Box>
+
+        {/* Feature 2 */}
+        <Box style={styles.featureBox}>
+            <div style={styles.imagecont}>
+          <img  style={styles.image}src={Images.scientist} alt="Feature 2" /></div>
+          <Typography variant="h6" style={styles.featurename}>Book a Master Health Check Up</Typography>
+          <Typography variant="body1">
+          Our access to the wide range of lab tests available.
+          </Typography>
+        </Box>
+        {/* Feature 3 */}
+        <Box style={styles.featureBox}>
+            <div style={styles.imagecont}>
+          <img  style={styles.image}src={Images.familyMember} alt="Feature 3" /></div>
+          <Typography variant="h6" style={styles.featurename}>Get access to Health Records</Typography>
+          <Typography variant="body1">
+          Book an online consultation or hospital visit with our in house specialties.
+          </Typography>
+        </Box>
+
+        {/* Feature 4 */}
+        <Box style={styles.featureBox}>
+            <div style={styles.imagecont}>
+          <img  style={styles.image}src={Images.familyMember} alt="Feature 4" /></div>
+          <Typography variant="h6" style={styles.featurename}>Add Your Family Member</Typography>
+          <Typography variant="body1">
+          Add your family members to the account and keep track.
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+            </Box>
+            <br></br>
+            <br></br>
+            <br></br>
             {/* Fourth Split */}
-            <Box style={styles.fourthSplit}>
+            <Box >
                 <Box style={styles.cardGrid}>
                     {[...Array(showMore ? 8 : 4)].map((_, index) => (
                         <Card key={index} style={styles.gridCard}>
